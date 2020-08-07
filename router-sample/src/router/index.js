14141ed106156762dab56236a523a02ec78a9193
ics-konno <konno@ics-web.jp>
2020-08-07 17:34:10 +0900

    NewPage.vueを作成し"/new"パスにリンクを作成

--- router-sample/src/router/index.js
+++ router-sample/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import NewPage from "../views/NewPage"
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/new",
+    name: "Name",
+    component: NewPage
   }
 ];
 
---
+++ router-sample/src/views/NewPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="new">
+    <h1>This is New Page!</h1>
+  </div>
+</template>
